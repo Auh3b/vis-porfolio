09b17a0f2299efa2546d1625f720c4c26c338d7a
Auh3b <ngaiyayec@yahoo.com>
2025-01-27 23:28:25 +0200

    added view icon

---
+++ src/components/customIcons/ViewIcon.tsx
@@ -0,0 +1,37 @@
+import { SvgIcon } from '@mui/material';
+
+export default function ViewIcon() {
+  return (
+    <SvgIcon viewBox='0 0 60 40'>
+      <rect
+        width={10}
+        height={10}
+        y={20}
+      />
+      <rect
+        width={10}
+        height={10}
+        x={25}
+        y={0}
+      />
+      <rect
+        width={10}
+        height={10}
+        x={25}
+        y={20}
+      />
+      <rect
+        width={10}
+        height={10}
+        x={25}
+        y={40}
+      />
+      <rect
+        width={10}
+        height={10}
+        x={50}
+        y={20}
+      />
+    </SvgIcon>
+  );
+}
