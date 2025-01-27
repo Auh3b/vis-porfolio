542b3c2146fb6b864556aa2411d8abca69383c7e
Auh3b <ngaiyayec@yahoo.com>
2025-01-27 23:28:11 +0200

    added icons

--- src/components/customIcons/DotArrow.tsx
+++ src/components/customIcons/DotArrow.tsx
@@ -1,3 +1,55 @@
-export default function DotArrow() {
-  return <svg></svg>;
+import { SvgIcon, SxProps } from '@mui/material';
+
+interface SVGSxProps {
+  sxProps?: SxProps;
+}
+
+export default function DotArrow(props: SVGSxProps) {
+  const { sxProps } = props;
+  return (
+    <SvgIcon
+      viewBox='0 0 40 60'
+      sx={sxProps}>
+      <rect
+        width={10}
+        height={10}
+      />
+      <rect
+        width={10}
+        height={10}
+        x={0}
+        y={20}
+      />
+      <rect
+        width={10}
+        height={10}
+        x={30}
+        y={20}
+      />
+      <rect
+        width={10}
+        height={10}
+        x={0}
+        y={40}
+      />
+    </SvgIcon>
+  );
+}
+
+export function LeftDotArrow(props: SVGSxProps) {
+  return (
+    <DotArrow sxProps={{ transform: 'rotate(0.5turn)', ...props.sxProps }} />
+  );
+}
+
+export function UpDotArrow(props: SVGSxProps) {
+  return (
+    <DotArrow sxProps={{ transform: 'rotate(0.75turn)', ...props.sxProps }} />
+  );
+}
+
+export function DownDotArrow(props: SVGSxProps) {
+  return (
+    <DotArrow sxProps={{ transform: 'rotate(0.75turn)', ...props.sxProps }} />
+  );
 }
---
+++ src/components/customIcons/Logo.tsx
@@ -0,0 +1,25 @@
+export default function Logo() {
+  return (
+    <svg
+      width='140'
+      height='60'
+      xmlns='http://www.w3.org/2000/svg'>
+      <rect
+        width={20}
+        height={20}
+      />
+      <rect
+        width={100}
+        height={20}
+        x={20}
+        y={20}
+      />
+      <rect
+        width={20}
+        height={20}
+        x={120}
+        y={40}
+      />
+    </svg>
+  );
+}
